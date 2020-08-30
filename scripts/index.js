00b6f51b0361ebb184af637201aa961df49c5379
Ivan Kondratyev <zm.fut@bk.ru>
2020-08-30 11:05:18 +0300

    fixed1

--- scripts/index.js
+++ scripts/index.js
@@ -3,11 +3,14 @@ let editButton=document.querySelector('.edit-button');
 let exitButton=document.querySelector('.popup__exit-button');
 let popupButton=document.querySelector('.popup__button');
 let formElement = document.querySelector('.popup__conteiner');
+let nameInput = document.querySelector('.popup__item-name');
+let jobInput = document.querySelector('.popup__item-activity');
+let profileName = document.querySelector('.profile__name');
+let profileActivity = document.querySelector('.profile__activity');
 
 function openPopup(){
     popup.classList.toggle('popup_opened')
 }
-
 
 editButton.addEventListener('click',openPopup);
 exitButton.addEventListener('click',openPopup);
@@ -15,15 +18,9 @@ popupButton.addEventListener('click',openPopup);
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
-    let nameInput = document.querySelector('.popup__item-name');
-    let jobInput = document.querySelector('.popup__item-activity');
-    
+
     nameInput=nameInput.value;
     jobInput=jobInput.value;
-    
-    
-    let profileName = document.querySelector('.profile__name');
-    let profileActivity = document.querySelector('.profile__activity');
 
     profileName.textContent = nameInput;
     profileActivity.textContent = jobInput;
